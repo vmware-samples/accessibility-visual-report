55c69edc23b18decf4992fda4adb680282eb9143
Peter Tan <tanp@vmware.com>
2022-04-24 20:23:52 +0800

    Accessibility Visual Report landing page

--- backend/accessibility/wsgi.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2022 VMware, Inc.
-# SPDX-License-Identifier: MIT
-
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE","accessibility.settings")
-application=get_wsgi_application()
--- frontend/src/environments/environment.test.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2022 VMware, Inc.
-// SPDX-License-Identifier: MIT
-
-export const environment = {
-    production: false,
-    apiServiceUrl: 'https://localhost/api',
-    baseUrl: 'http://localhost:8000'
-  };
